c17bb7baf88f86e9396d53ebf5c791e99ffd7670
pzuritas <pzurita@uc.cl>
2026-05-29 14:29:09 +0100

    🏗️ update module

--- src/FFCM.jl
+++ src/FFCM.jl
@@ -7,9 +7,11 @@ using LinearAlgebra: mul!
 
 include("config.jl")
 include("cell_list.jl")
+include("stencil.jl")
 include("spread_forces.jl")
 include("stokes_solve.jl")
+include("interpolate.jl")
 
-export FFCMConfig, spread_forces!, stokes_solve!
+export FFCMConfig, spread_forces!, stokes_solve!, interpolate_velocities!
 
 end
